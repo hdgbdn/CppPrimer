f4baa489af53e7a4e3c85e1bd0d190aa723d2dc0
HuDi <hdgbdn92@gmail.com>
2021-06-22 17:57:05 +0800

    excrise 13.1.1: add 13.4 13.5

---
+++ src/13_Copy_Control/13_01_02_excrise.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // 13.6
+    // copy-assignment is the '=' operator used to control how objects of its class are assigned.
+
+    // 13.7
+    // For StrBlob: the shared_ptr is copied, which is expected, and point to the same vector<string>
+    // For StrBlobPtr: the shared_ptr is copied, and point to the same vector<string>
+
+    // 13.8
+    {
+        
+    }
+    return 0;
+}
