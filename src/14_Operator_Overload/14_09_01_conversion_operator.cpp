f7c32e7ecf93bfd4cb7e971ec6117117e2abf4ec
HuDi <hdgbdn92@gmail.com>
2021-07-03 09:36:41 +0800

    chapter 14.9.1

---
+++ src/14_Operator_Overload/14_09_01_conversion_operator.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class SmallInt{
+public:
+    SmallInt(int i = 0): val(i)
+    {
+        if(i < 0 || i > 255)
+            throw out_of_range("Bad SmallInt value");
+    }
+    operator int() const { cout << "conversion to int: " << to_string(val) << endl; return val; }
+private:
+    size_t val;
+};
+
+class ExpilicSmallInt{
+public:
+ExpilicSmallInt(int i = 0): val(i)
+{
+    if (i < 0 || i > 255)
+        throw out_of_range("Bad SmallInt value");
+}
+explicit operator int() const
+{
+    cout << "explicit conversion to int: " << to_string(val) << endl;
+    return val;
+}
+
+private:
+size_t val;
+};
+
+int main()
+{
+    // a special member function
+    SmallInt si;
+    si = 4;     // implicit covert 4 to SmallInt
+    si + 3;     // convert si to int
+    SmallInt sd = 3.14; // 3.14 convert to int 3
+    cout << "si + 3.14: is" << si + 3.14;          // convert si to int, and to double
+
+    // exception
+    // cin << i;        if istream can convert to bool, then the coverted bool will be promted as int, then becames 1 << i
+    // explicit conversion operator:
+    ExpilicSmallInt esi = 3;
+    // esi + 3;                // no operator "+" matches these operands -- operand types are: ExpilicSmallInt + int
+    static_cast<int>(esi) + 3;  // explicit conversion to int: 3
+
+    // convert to bool
+    string value;
+    while(std::cin >> value);       // cin has a istream operator bool, if instream is good, then return true;
+    cout << "finish input" << endl;
+
+    return 0;
+}
