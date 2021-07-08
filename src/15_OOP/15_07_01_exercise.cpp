ac925deb1a6d6bc868c7aa8ec4126265f396575f
HuDi <hdgbdn92@gmail.com>
2021-07-08 09:20:58 +0800

    chapter&exercise 15.07.01

---
+++ src/15_OOP/15_07_01_exercise.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // 15.24
+    // a class may be dervided needs a virtual destructor, because may need to destruct a dervied object by a base pointer/reference
+    // a virtual destructor need to free the dynamic allocated memories
+    return 0;
+}
---
+++ src/15_OOP/15_07_01_virtual_destructor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Base{
+public:
+    virtual ~Base(){ cout << "Base::~Base()" << endl;}
+};
+
+class Derived: public Base{
+public:
+    virtual ~Derived(){ cout << "Dervied::~Derived()" << endl; }
+};
+
+int main()
+{
+    // why need virtual destructor
+    // because if delete a base pointer which points to a derived object,
+    // then if we don't define virtual destructor, then it will use the base destructor
+    Base* p = new Derived();
+    delete p;
+    // output is:
+    // Dervied::~Derived()
+    // Base::~Base()
+    // looks like the base part's destructor is automatically called
+    return 0;
+}
